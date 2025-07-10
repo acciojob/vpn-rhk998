9f8e5debb999bbf6cf202ec31dc90acb82414838
rhk998 <hemanthk.998r@gmail.com>
2025-07-11 00:53:05 +0530

    adminregister

--- src/main/java/com/driver/services/impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -31,54 +31,30 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User register(String username, String password, String countryName) throws Exception{
-        String countryName1 = countryName.toUpperCase();
-
-        if (!countryName1.equals("IND") && !countryName1.equals("USA") && !countryName1.equals("CHI") && !countryName1.equals("JPN"))
+        CountryName countryEnum;
+        try {
+            countryEnum = CountryName.valueOf(countryName.toUpperCase());
+        } catch (IllegalArgumentException e) {
             throw new Exception("Country not found");
-        Country country = new Country(CountryName.valueOf(countryName1.toString()),
-                                        CountryName.valueOf(countryName1).toCode());
-
+        }
 
         User user = new User();
         user.setUsername(username);
         user.setPassword(password);
         user.setConnected(false);
 
+        Country country = new Country();
+        country.setCountryName(countryEnum);
+        country.setCode(countryEnum.toCode());
         country.setUser(user);
+
         user.setOriginalCountry(country);
 
-        user = userRepository3.save(user);
+        User savedUser = userRepository3.save(user);
+        savedUser.setOriginalIp(country.getCode() + "." + savedUser.getId());
 
-        user.setOriginalIp(new String(user.getOriginalCountry().getCode() + "." + user.getId()));
-
-        user = userRepository3.save(user);
-        return user;
+        return userRepository3.save(savedUser);
     }
-//    public User register(String username, String password, String countryName) throws Exception{
-//        CountryName countryEnum;
-//        try {
-//            countryEnum = CountryName.valueOf(countryName.toUpperCase());
-//        } catch (IllegalArgumentException e) {
-//            throw new Exception("Country not found");
-//        }
-//
-//        User user = new User();
-//        user.setUsername(username);
-//        user.setPassword(password);
-//        user.setConnected(false);
-//
-//        Country country = new Country();
-//        country.setCountryName(countryEnum);
-//        country.setCode(countryEnum.toCode());
-//        country.setUser(user);
-//
-//        user.setOriginalCountry(country);
-//
-//        User savedUser = userRepository3.save(user);
-//        savedUser.setOriginalIp(country.getCode() + "." + savedUser.getId());
-//
-//        return userRepository3.save(savedUser);
-//    }
 
     @Override
     public User subscribe(Integer userId, Integer serviceProviderId) {
