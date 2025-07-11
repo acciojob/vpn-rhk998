d0153ba6b9fcb43152ce680125434b3a1e8ee7b2
rhk998 <hemanthk.998r@gmail.com>
2025-07-11 12:53:06 +0530

    userAdmin

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -1,10 +1,12 @@
 package com.driver.services.impl;
 
-import com.driver.model.*;
+import com.driver.model.Admin;
+import com.driver.model.Country;
+import com.driver.model.CountryName;
+import com.driver.model.ServiceProvider;
 import com.driver.repository.AdminRepository;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
-import com.driver.repository.UserRepository;
 import com.driver.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,22 +17,14 @@ import java.util.Optional;
 
 @Service
 public class AdminServiceImpl implements AdminService {
+    @Autowired
+    AdminRepository adminRepository1;
 
-    private final UserRepository userRepository1;
-    private final ServiceProviderRepository serviceProviderRepository1;
-    private final CountryRepository countryRepository1;
-    private final AdminRepository adminRepository1;
+    @Autowired
+    ServiceProviderRepository serviceProviderRepository1;
 
-    // ✅ Constructor-based injection
-    public AdminServiceImpl(UserRepository userRepository3,
-                           ServiceProviderRepository serviceProviderRepository3,
-                           CountryRepository countryRepository3,
-                            AdminRepository adminRepository1) {
-        this.userRepository1 = userRepository3;
-        this.serviceProviderRepository1 = serviceProviderRepository3;
-        this.countryRepository1 = countryRepository3;
-        this.adminRepository1 = adminRepository1;
-    }
+    @Autowired
+    CountryRepository countryRepository1;
 
     @Override
     public Admin register(String username, String password) {
@@ -57,29 +51,29 @@ public class AdminServiceImpl implements AdminService {
         return null;
     }
 
-//
-@Override
-public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
-    //add a country under the serviceProvider and return respective service provider
+    @Override
+    public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
+        //add a country under the serviceProvider and return respective service provider
 
-    // You should create a new Country object based on the given country name and add it to the country list of the service provider.
-    // Note that the user attribute of the country in this case would be null.
-    //In case country name is not amongst the above mentioned strings, throw "Country not found" exception
-    boolean isCountryPresent = false;
-    String str = countryName.toUpperCase();
-    if(!str.equals("IND") && !str.equals("JPN") && !str.equals("AUS") && !str.equals("CHI") && !str.equals("USA")){
-        throw  new Exception("Country not found");
-    }
-    ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
+        // You should create a new Country object based on the given country name and add it to the country list of the service provider.
+        // Note that the user attribute of the country in this case would be null.
+        //In case country name is not amongst the above mentioned strings, throw "Country not found" exception
+        boolean isCountryPresent = false;
 
-    Country country = new Country(CountryName.valueOf(str), CountryName.valueOf(str).toCode());
+        String str = countryName.toUpperCase();
+        if(!str.equals("IND") && !str.equals("JPN") && !str.equals("AUS") && !str.equals("CHI") && !str.equals("USA")){
+            throw  new Exception("Country not found");
+        }
+        ServiceProvider serviceProvider = serviceProviderRepository1.findById(serviceProviderId).get();
+
+        Country country = new Country(CountryName.valueOf(str), CountryName.valueOf(str).toCode());
 
 
-    country.setServiceProvider(serviceProvider);
-    serviceProvider.getCountryList().add(country);
+        country.setServiceProvider(serviceProvider);
+        serviceProvider.getCountryList().add(country);
 
-    serviceProviderRepository1.save(serviceProvider);
+        serviceProviderRepository1.save(serviceProvider);
 
-    return serviceProvider;
+        return serviceProvider;
     }
 }
--- src/main/java/com/driver/services/impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -1,9 +1,9 @@
 package com.driver.services.impl;
 
-import com.driver.model.Country;
 import com.driver.model.CountryName;
 import com.driver.model.ServiceProvider;
 import com.driver.model.User;
+import com.driver.model.Country;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
 import com.driver.repository.UserRepository;
@@ -11,23 +11,15 @@ import com.driver.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class UserServiceImpl implements UserService {
 
-    private final UserRepository userRepository3;
-    private final ServiceProviderRepository serviceProviderRepository3;
-    private final CountryRepository countryRepository3;
-
-    // ✅ Constructor-based injection
-    public UserServiceImpl(UserRepository userRepository3,
-                           ServiceProviderRepository serviceProviderRepository3,
-                           CountryRepository countryRepository3) {
-        this.userRepository3 = userRepository3;
-        this.serviceProviderRepository3 = serviceProviderRepository3;
-        this.countryRepository3 = countryRepository3;
-    }
+    @Autowired
+    UserRepository userRepository3;
+    @Autowired
+    ServiceProviderRepository serviceProviderRepository3;
+    @Autowired
+    CountryRepository countryRepository3;
 
     @Override
     public User register(String username, String password, String countryName) throws Exception{
@@ -57,18 +49,15 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User subscribe(Integer userId, Integer serviceProviderId) {
-         User user = userRepository3.findById(userId).get();
+
         ServiceProvider serviceProvider = serviceProviderRepository3.findById(serviceProviderId).get();
+        User user = userRepository3.findById(userId).get();
 
-        List<User> users = serviceProvider.getUsers();
-        users.add(user);
-        serviceProvider.setUsers(users);
+        user.getServiceProviderList().add(serviceProvider);
+        serviceProvider.getUsers().add(user);
+
         serviceProviderRepository3.save(serviceProvider);
+        return user;
 
-        List<ServiceProvider> providers = user.getServiceProviderList();
-        providers.add(serviceProvider);
-        user.setServiceProviderList(providers);
-
-        return userRepository3.save(user);
     }
 }
